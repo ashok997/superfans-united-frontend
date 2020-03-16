7fcaf898cfb5c2e89d5766c062743b8622401d58
ashok997 <ashok997@gmail.com>
2020-03-16 10:18:29 -0500

    characters container changed to display images and display related comments and votes

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -7,9 +7,13 @@ const Characters = (props) => {
             {props.characters.map(character =>
                 <li key={character.id}>
                     <h1>{character.name}</h1>
-                    {character.description}
-                    <p>{character.thumbnail}</p>
-                    <p>{character.image}</p>
+                    <p>{character.description}</p>
+                    <img src={character.thumbnail} alt="thumbnail" width="200" height="200" />
+                    {character.user_characters.map(entry =>
+                        <div><p> Votes: {entry.votes}</p>
+                            <p> Comments: {entry.comments} By: {entry.user_id}</p>
+                        </div>
+                    )}
                 </li>)}
         </div>
     )
