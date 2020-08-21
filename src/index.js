73bef7e6d0fee19f5bff2b961e3e72b08f6c433c
ashok997 <ashok997@gmail.com>
2020-08-21 15:01:17 -0500

    added currentUser reducer and used combine reducers in index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import characterReducer from './reducers/characterReducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import characterReducer from "./reducers/characterReducer";
+import currentUser from "./reducers/currentUser";
 
-import App from './App';
+import App from "./App";
+
+const reducer = combineReducers({
+  characterReducer,
+  currentUser,
+});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-let store = createStore(characterReducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <Route path="/" component={App} />
-        </Router>
-    </Provider>,
-    document.getElementById('root'));
-
-
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
