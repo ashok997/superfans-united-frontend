72385cbef459e9f81172855c1f6e7d0ac132a697
ashok997 <ashok997@gmail.com>
2020-09-07 12:46:43 -0500

    added get current user action

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -24,3 +24,22 @@ export function login(credentials) {
       });
   };
 }
+
+export function getCurrentUser() {
+  return (dispatch) => {
+    fetch("http://localhost:3001/api/v1/get_current_user", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.error) {
+          alert(user.error);
+        } else {
+          dispatch(setCurrentUser(user));
+        }
+      });
+  };
+}
