062ac0ff7244cb0ae002c942cca261cd106750b5
ashok997 <ashok997@gmail.com>
2020-10-03 13:46:44 -0500

    used .env to process marvel api keys

--- src/actions/fetchCharactersFromApi.js
+++ src/actions/fetchCharactersFromApi.js
@@ -1,9 +1,9 @@
 export function fetchCharactersFromApi(nameStartsWith) {
-  const MARVEL_API_KEY = "ee02c6187f39e0e74f486af6be409b19";
+  const REACT_APP_MARVEL_API_KEY = process.env.REACT_APP_MARVEL_API_KEY;
   const URL =
     "https://gateway.marvel.com:443/v1/public/characters?" +
     `nameStartsWith=${nameStartsWith}` +
-    `&apikey=${MARVEL_API_KEY}`;
+    `&apikey=${REACT_APP_MARVEL_API_KEY}`;
 
-  return fetch(URL).then(response => response.json());
+  return fetch(URL).then((response) => response.json());
 }
